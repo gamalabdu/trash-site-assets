91d2079b533c3dea62eb2c103293fde86ca1a91a
gamalabdu <abdu.s.gamal@gmail.com>
2024-06-30 14:28:02 -0400

    new asset creations page

--- schemas/index.ts
+++ schemas/index.ts
@@ -6,5 +6,6 @@ import productionPage from "./productionPage"
 import productionPageAlbums from "./productionPageAlbums"
 import song from "./song"
 import work from "./work"
+import assetCreationPage from "./assetCreationPage"
 
-export const schemaTypes = [song, work, home, enter, forartist, productionPage, productionPageAlbums, brandingPage]
+export const schemaTypes = [song, work, home, enter, forartist, productionPage, productionPageAlbums, brandingPage, assetCreationPage]
